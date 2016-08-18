e90e3f754dfc4a855787b8943e1ac7a93317d0b9
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-18 00:10:08 -0700

    this is my quickfix branch and i will use to do my quickfixes

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,10 +2,26 @@ package controllers
 
 import "github.com/revel/revel"
 
+
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	Portfolio := "Portfolio's"
+	name := "Gentry"
+
+	return c.Render(Portfolio, name)
+}
+func (c App) Hello(name string) revel.Result{
+	c.Validation.Required(name).Message("Your name is required!")
+	c.Validation.MinSize(name, 3).Message("Your name is not long enough!")
+
+if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+}
+
+	return c.Render(name)
 }
